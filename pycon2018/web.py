68a711ca2e822929325d239229ee9a67c2baaa65
Swen Mun <swen@spoqa.com>
2018-08-10 16:29:54 +0900

    linting

--- pycon2018/web.py
+++ pycon2018/web.py
@@ -15,7 +15,6 @@ from sqlalchemy.orm.session import Session
 from sqlalchemy.sql.expression import or_
 from sqlalchemy.sql.functions import now
 from werkzeug.local import LocalProxy
-from werkzeug.routing import BaseConverter
 from werkzeug.urls import url_decode
 
 from .app import App
@@ -215,7 +214,7 @@ def test_submission():
         'data': data
     }
     return jsonify(result='success', match=match)
-    
+
 
 @ep.route('/tournaments/<uuid:tournament_id>/submission', methods=['POST'])
 @login_required
@@ -271,7 +270,7 @@ def tournament(tournament_id: uuid.UUID):
     if tournament.final_match:
         tree = build_match_tree(tournament.final_match)
     else:
-        tree =None
+        tree = None
     return render_template('admin/tournament.html', tournament=tournament,
                            submissions_without_match=submissions_without_match,
                            tree=tree, enumerate=enumerate, range=range)
@@ -458,4 +457,3 @@ def create_web_app(app: App) -> Flask:
     if app.sentry_dsn:
         Sentry(wsgi, dsn=app.sentry_dsn)
     return wsgi
-
